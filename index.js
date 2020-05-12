da065405fbd74ef581c62cb984bb1d64a0f977b2
DiegoHZ5 <safairus@gmail.com>
2020-05-12 10:33:35 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,25 +5,26 @@ app.listen(app.get('port'),() => {
 	console.log("El servidor está en: ", app.get('port'));
 });
 
-// const { Blockchain, Transaccion} = require('./blockchain');
-// const EC = require('elliptic').ec;
-// const ec = new EC('secp256k1');
+const { Blockchain, Transaccion} = require('./blockchain');
+const EC = require('elliptic').ec;
+const ec = new EC('secp256k1');
+const miLlave = ec.keyFromPrivate('772dcf672d039840baa294b8aaa5f6e6f288bcd5da1c2648f8e10fcbdf3d6dfa');
+const cartera = miLlave.getPublic('hex');
 
-// const miLlave = ec.keyFromPrivate('772dcf672d039840baa294b8aaa5f6e6f288bcd5da1c2648f8e10fcbdf3d6dfa');
-// const cartera = miLlave.getPublic('hex');
+let digiCoin = new Blockchain();
 
-// let digiCoin = new Blockchain();
-// const tx1 = new Transaccion(cartera,'llavePublicaDeAlguien',10);
-// tx1.signTransaccion(miLlave);
-// digiCoin.agregarTransaccion(tx1);
-// digiCoin.minarTransaccionesPendientes(cartera);
+console.log("Minando.....");
+console.log("----------------------------------------------------------------")
+console.log("Balance de minero es: " + digiCoin.getDinero(cartera));
+console.log("----------------------------------------------------------------")
+digiCoin.cambiarDificultad(2);
+const tx1 = new Transaccion(cartera,'llavePublicaDeAlguien',80);
+tx1.signTransaccion(miLlave);
+digiCoin.agregarTransaccion(tx1);
+digiCoin.minarTransaccionesPendientes(cartera);
 
-// const tx2 = new Transaccion(cartera,'llavePublicaDeAlguien',20);
-// tx2.signTransaccion(miLlave);
-// digiCoin.agregarTransaccion(tx2);
-
-// console.log("Minando.....");
-// digiCoin.minarTransaccionesPendientes(cartera);
-// console.log("Balance de minero es: " + digiCoin.getDinero(cartera));
-
-// console.log("Es la cadena valida???: " + digiCoin.CadenaEsValida());
+console.log("----------------------------------------------------------------")
+console.log("Es la cadena valida???: " + digiCoin.CadenaEsValida());
+console.log("----------------------------------------------------------------")
+console.log("Balance de minero es: " + digiCoin.getDinero(cartera));
+console.log("----------------------------------------------------------------")
