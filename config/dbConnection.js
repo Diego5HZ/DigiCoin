82fa6458fc0d96033b7c89cad64a3b89529c10c8
DiegoHZ5 <safairus@gmail.com>
2020-05-12 00:35:54 -0500

    Update dbConnection.js

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -2,8 +2,8 @@ const mysql = require('mysql');
 module.exports = () => {
 	return mysql.createConnection({
 		host: 'us-cdbr-east-06.cleardb.net',
-		user: 'b016ae25b95b0d',
-		password: '49858173',
-		database: 'heroku_ab9f3e784f1601c'
+		user: 'ba0ae1386dc485',
+		password: 'e11c1ddc',
+		database: 'heroku_3167f95de7bdfc6'
 	});
 }
